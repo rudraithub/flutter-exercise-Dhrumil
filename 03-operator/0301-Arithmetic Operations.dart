2551b6ecc050e10ec5ccca018953c7ff7fbb7889
Dhrumil2611 <dhrumilasal0707@gmail.com>
2024-03-22 10:47:41 +0530

    update

---
+++ 03-operator/0301-Arithmetic Operations.dart
@@ -0,0 +1,38 @@
+void main(){
+ int a= 56;
+  int b=80;
+
+print(a+b);
+}
+
+void main (){
+int a=56;
+int b=80;
+
+print(a-b);
+}
+
+void main(){
+int a=56;
+int b=80;
+
+print(a*b);
+}
+
+void main (){
+int a=56;
+int b=80;
+
+print(a/b);
+}
+
+void main(){
+int a= 56;
+int b=80;
+
+print(a%b);
+}
+
+
+
+
