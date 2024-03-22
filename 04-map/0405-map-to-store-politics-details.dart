f5193398173020b3085e6282f93687e3c4cf459c
Dhrumil2611 <dhrumilasal0707@gmail.com>
2024-03-22 14:39:45 +0530

    update

---
+++ 04-map/0405-map-to-store-politics-details.dart
@@ -0,0 +1,11 @@
+void main() {
+  Map<String, dynamic> map5 = {
+    "president": "sri draupadi murmu",
+    "primeMinister": "narendra modi",
+    "totalSeats": 25,
+    "oppositionParties": "congress,AAP",
+    "governmentType": "state goverment",
+    "City": "bhavnagar"
+  };
+  print(map5);
+}
