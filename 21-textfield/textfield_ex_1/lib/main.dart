4c93aa8fb201416d2ad14ace4ee90cb4438d693c
Dhrumil2611 <dhrumilasal0707@gmail.com>
2024-06-21 14:54:48 +0530

    update

---
+++ 21-textfield/textfield_ex_1/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: demo(),
+    );
+  }
+}
+
+class demo extends StatefulWidget {
+  const demo({super.key});
+
+  @override
+  State<demo> createState() => _demoState();
+}
+
+class _demoState extends State<demo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("text field demo"),
+      ),
+      body: TextField(
+        decoration: InputDecoration(
+          hintText: "enter your name",
+          prefixIcon: Icon(Icons.person)),
+        
+      ),
+    );
+  }
+}
