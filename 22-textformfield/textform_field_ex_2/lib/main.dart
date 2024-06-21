7be64d767cfec76562e5df99b2b007da2b31b120
Dhrumil2611 <dhrumilasal0707@gmail.com>
2024-06-21 15:22:19 +0530

    up

--- 22-textformfield/textform_field_ex_2/lib/main.dart
+++ 22-textformfield/textform_field_ex_2/lib/main.dart
@@ -27,7 +27,7 @@ class _demoState extends State<demo> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 241, 155, 43),
+        backgroundColor: Color.fromARGB(255, 192, 28, 233),
         title: Text(
           "textform field demo",
           style: TextStyle(color: const Color.fromARGB(255, 17, 17, 17)),
@@ -38,11 +38,11 @@ class _demoState extends State<demo> {
           child: TextFormField(
             decoration: InputDecoration(
                 enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.yellow)),
+                    borderSide: BorderSide(color: Color.fromARGB(255, 184, 69, 236))),
                 focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.black12)),
                 labelText: "enter your username",
-                prefixIcon: Icon(Icons.person,color: Colors.amberAccent,)),
+                prefixIcon: Icon(Icons.person,color: Color.fromARGB(255, 230, 49, 236),)),
           ),
         ),
       
