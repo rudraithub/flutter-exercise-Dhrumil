035a2eee535ad2ac0f2458d8dbafac6f73ecc675
Dhrumil2611 <dhrumilasal0707@gmail.com>
2024-06-27 10:50:50 +0530

    update

---
+++ 31-checkbox/checkbox_ex_2/android/app/src/main/kotlin/com/example/checkbox_ex_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.checkbox_ex_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ 31-checkbox/checkbox_ex_2/lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myapp());
+}
+
+class myapp extends StatelessWidget {
+  const myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+      home: demo(),
+    );
+  }
+}
+
+class demo extends StatefulWidget {
+  const demo({super.key});
+
+  @override
+  State<demo> createState() => _demoState();
+}
+
+class _demoState extends State<demo> {
+  bool a = false;
+  bool c = false;
+   bool b = false;
+    bool d = false;
+    bool x = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              Checkbox(
+                  value: c,
+                  onChanged: (newvalue) {
+                    setState(() {
+                      c = newvalue!;
+                    });
+                  }),
+              Text("travel",style: TextStyle(color: Colors.red),),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              Checkbox(
+                  value: b,
+                  onChanged: (newvalue) {
+                    setState(() {
+                      b = newvalue!;
+                    });
+                  }),
+              Text("music",style: TextStyle(color: Colors.red),),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              Checkbox(
+                  value: d,
+                  onChanged: (newvalue) {
+                    setState(() {
+                    d= newvalue!;
+                    });
+                  }),
+              Text("shopping",style: TextStyle(color: Colors.red),),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              Checkbox(
+                  value: a,
+                  onChanged: (Cvalue) {
+                    setState(() {
+                      a = Cvalue!;
+                    });
+                  }),
+              Text("reading",style: TextStyle(color: Colors.red),),
+            ],
+          ),
+             Row(
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              Checkbox(
+                  value: x,
+                  onChanged: (Cvalue) {
+                    setState(() {
+                      x= Cvalue!;
+                    });
+                  }),
+              Text("swimming",style: TextStyle(color: Colors.red),),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
