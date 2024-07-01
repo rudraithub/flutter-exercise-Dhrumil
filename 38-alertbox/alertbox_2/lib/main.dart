9fbdecd12e818e29d950a275f0784a1d59a97bcf
Dhrumil2611 <dhrumilasal0707@gmail.com>
2024-07-01 16:26:29 +0530

    update

---
+++ 38-alertbox/alertbox_2/lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myapp());
+}
+
+class myapp extends StatelessWidget {
+  const myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: demo(),
+    );
+  }
+}
+
+class demo extends StatefulWidget {
+  const demo({super.key});
+
+  @override
+  State<demo> createState() => _demoState();
+}
+
+class _demoState extends State<demo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 214, 135, 31),
+        title: Text("Alert Demo",style: TextStyle(color: Colors.green),),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(5, 10, 5, 5),
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero)),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                            title: Text("Alert"),
+                            content: Text("Do you want to exit ?"),
+                            actions: [
+                              TextButton(
+                                  style: TextButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.zero),
+                                      backgroundColor: Color.fromARGB(255, 175, 27, 35),),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text("cancle",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white))),
+                              TextButton(
+                                  style: TextButton.styleFrom(
+                                      backgroundColor: Color.fromARGB(255, 175, 27, 35),
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.zero)),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text(
+                                    "Ok",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white),
+                                  ))
+                            ],
+                          ));
+                },
+                child: Text(
+                  "   Show Dialog   ",
+                  style: TextStyle(
+                      color: Colors.white, backgroundColor: Colors.grey),
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
