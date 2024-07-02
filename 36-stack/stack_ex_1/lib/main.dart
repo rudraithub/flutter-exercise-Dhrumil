e3e4432b5b22cb77ffe8dd9bfa509200c33034bf
Dhrumil2611 <dhrumilasal0707@gmail.com>
2024-07-02 15:46:52 +0530

    update

--- 36-stack/stack_ex_1/lib/main.dart
+++ 36-stack/stack_ex_1/lib/main.dart
@@ -9,7 +9,7 @@ class myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( 
       debugShowCheckedModeBanner: false,
       home: demo(),
     );
--- 40-bottomsheet/bottomsheet_ex_1/lib/main.dart
+++ 40-bottomsheet/bottomsheet_ex_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(myapp());
@@ -27,7 +28,55 @@ class _demoState extends State<demo> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ,
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.zero))),
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ListTile(
+                          title: Text(
+                            textAlign: TextAlign.center,
+                            "rudra it hub",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 12, 12, 12)),
+                          ),
+                        ),
+                        TextButton(
+                            style: TextButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero),
+                                backgroundColor: Colors.green),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text(
+                              "Close Bottom sheet",
+                              style: TextStyle(color: Colors.white),
+                            )),
+                        ListTile(
+                          title: Text(
+                            textAlign: TextAlign.center,
+                            "I Am Bottomsheet",
+                            style:
+                                TextStyle(color: Color.fromARGB(255, 8, 8, 8)),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                  backgroundColor: Color.fromARGB(255, 155, 211, 157));
+            },
+            child: Text(
+              "Show bottom sheet",
+              style: TextStyle(color: Colors.white),
+            )),
       ),
     );
   }
