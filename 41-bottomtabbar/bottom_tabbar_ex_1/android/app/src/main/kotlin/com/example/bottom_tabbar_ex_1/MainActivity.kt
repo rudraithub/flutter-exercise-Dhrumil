b4b48d89607c11038ca470308614d30ccecf5ee7
Dhrumil2611 <dhrumilasal0707@gmail.com>
2024-07-09 17:55:36 +0530

    update

---
+++ 41-bottomtabbar/bottom_tabbar_ex_1/android/app/src/main/kotlin/com/example/bottom_tabbar_ex_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bottom_tabbar_ex_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ 43-sliverappbar-sliverlist/sliver_appbar_list_ex_1/android/app/src/main/kotlin/com/example/sliver_appbar_list_ex_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sliver_appbar_list_ex_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
